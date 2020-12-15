ee7f79fce5b6e46576a1fd0ee8ac43280538e392
ehab wasel <ehabgithub@gmail.com>
2020-12-15 17:17:15 +0200

    attached the project files

--- Week3/project/index.js
+++ Week3/project/index.js
@@ -1,3 +1,46 @@
 // Your code goes in here
 
-document.querySelector("#app").innerText = "Tip Calculator";
+
+
+//Custom function
+function calculateTip(){
+
+
+  //Store data of inputs
+  let billAmount = document.getElementById("billAmount").value;
+  let serviceQuality = document.getElementById("serviceQuality").value;
+  let numPeople = document.getElementById("totalPeople").value;
+
+  //Quick validation
+  if(billAmount === "" || serviceQuality === 0){
+    return window.alert("Please enter some values to get this baby up and running!"); //this will prevent the function from continuing
+  }
+
+  //Check to see if this input is empty or less than or equal to 1
+  if(numPeople === "" || numPeople <= 1){
+    numPeople = 1;
+
+    document.getElementById("each").style.display = "none";
+
+  } else {
+
+    document.getElementById("each").style.display = "block";
+
+  }
+
+  
+  let  total = (parseFloat(billAmount)+parseFloat (serviceQuality)) / numPeople;
+  console.log(total)
+  
+
+  //Display the tip!
+  document.getElementById("totalTip").style.display = "block";
+  document.getElementById("tip").innerText = total;
+}
+
+//Hide the  tip amount on load
+document.getElementById("totalTip").style.display = "none";
+document.getElementById("each").style.display = "none";
+
+// Clicking the button calls our custom function
+document.getElementById("calculate").onclick = function(){ calculateTip(); }
